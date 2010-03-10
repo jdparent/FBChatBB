014ad550faf0ebfbcf35b7f82d4b81010c5d6ab0
Jeff Parent <jeff.parent@gmail.com>
2010-03-09 20:52:27 -0600

    Created initial JDE project. Included JXA 1.0 library.
    Signed-off-by: Jeff Parent <jeff.parent@gmail.com>

---
+++ org/biff/fbchatbb/FBChatBB.java
@@ -0,0 +1,20 @@
+/*
+ * FBChatBB.java
+ *
+ * FBChatBB: Facebook Chat application for the BlackBerry Smart Phone.
+ * 
+ * © Jeff Parent, 2010
+ * Confidential and proprietary.
+ */
+
+package org.biff.fbchatbb;
+
+
+
+
+/**
+ * 
+ */
+class FBChatBB {
+    FBChatBB() {    }
+} 
